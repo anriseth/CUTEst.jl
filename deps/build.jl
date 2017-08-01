d25ff5f08e5b1070698796845e22fad8b9693f39
Dominique Orban <dominique.orban@gmail.com>
2017-08-01 12:49:16 -0400

    use linuxbrew to build on linux

--- deps/build.jl
+++ deps/build.jl
@@ -4,16 +4,15 @@ using BinDeps
 
 libgsl = library_dependency("libgsl", aliases=["libgsl-0"])
 
-# package managers - currently with error on sudo
-#   https://discourse.julialang.org/t/installing-dependencies-error-sudo-no-tty-present-and-no-askpass-program-specified/778
-#provides(AptGet, Dict("libgsl0ldbl"=>libgsl, "libgsl0-dev" =>libgsl, "gsl-bin"=>libgsl))
-#provides(Yum, "gsl-devel", libgsl)
-#provides(Pacman, "gsl", libgsl)
-
 if is_apple()
     using Homebrew
     provides(Homebrew.HB, "gsl@1", libgsl, os = :Darwin)
 end
+if is_linux()
+    using Linuxbrew
+    provides(Linuxbrew.LB, "gsl@1", libgsl, os = :Linux)
+end
+Brew = is_apple() ? Homebrew : Linuxbrew
 
 # build from source
 provides(Sources, URI("http://ftp.gnu.org/gnu/gsl/gsl-1.16.tar.gz"), libgsl)
@@ -60,68 +59,39 @@ if validate_libcutest()
   check_env()
   ispath(cutestenv) && rm(cutestenv)
 else
-  @static if is_apple()
-    install = true
-    if isfile(cutestenv)
-      include(cutestenv)
-      if validate_libcutest()
-        info("Updating CUTEst")
-        Homebrew.brew(`update cutest`)
-        Homebrew.brew(`update mastsif`)
-        install = false
-      end
+  install = true
+  if isfile(cutestenv)
+    include(cutestenv)
+    if validate_libcutest()
+      info("Updating CUTEst")
+      Brew.brew(`update cutest`)
+      Brew.brew(`update mastsif`)
+      install = false
     end
+  end
 
-    if install
-      info("Installing CUTEst")
-      Homebrew.add("optimizers/cutest/cutest")
-      Homebrew.add("optimizers/cutest/mastsif")
+  if install
+    info("Installing CUTEst")
+    Brew.add("optimizers/cutest/cutest")
+    Brew.add("optimizers/cutest/mastsif")
 
-      open(cutestenv, "w") do cenv
-        for p in ["archdefs", "cutest", "sifdecode", "mastsif"]
-          prefix = Homebrew.prefix(p)
-          open("$prefix/$p.bashrc") do f
-            for line in readlines(f)
-              var = split(split(line, "=")[1])[2]
-              path = chomp(split(line, "=")[2])
-              write(cenv, "ENV[\"$var\"] = \"$path\"\n")
-            end
-          end
-        end
-        cd(here) do
-          isdir("files") && rm("files", recursive=true)
-          mkdir("files")
-        end
-        path = joinpath(here, "files", "problems")
-        println(cenv, "ENV[\"cutest-problems\"] = \"$path\"")
-      end
-    end
-  elseif is_linux()
-    cd(here) do
-      isdir("files") || mkdir("files")
-      cd("files") do
-        info("Installing CUTEst")
-
-        lnxurl = "https://raw.githubusercontent.com/abelsiqueira/linux-cutest/v0.3.1/install.sh"
-        run(`wget $lnxurl -O install.sh`)
-        ENV["C_INCLUDE_PATH"] = joinpath(here, "usr", "include")
-        run(`bash install.sh`)
-
-        open(cutestenv, "w") do cenv
-          open("cutest_env.bashrc") do f
-            for p in ["PATH", "MANPATH", "LD_LIBRARY_PATH"]
-              write(cenv, "$p = get(ENV, \"$p\", \"\")\n")
-            end
-            for line in readlines(f)
-              chomp(line) == "" && continue
-              var = split(split(line, "=")[1])[2]
-              path = chomp(split(line, "=")[2])
-              write(cenv, "ENV[\"$var\"] = \"$path\"\n")
-            end
-            println(cenv, "ENV[\"cutest-problems\"] = \"$(pwd())/problems\"")
+    open(cutestenv, "w") do cenv
+      for p in ["archdefs", "cutest", "sifdecode", "mastsif"]
+        prefix = Brew.prefix(p)
+        open("$prefix/$p.bashrc") do f
+          for line in readlines(f)
+            var = split(split(line, "=")[1])[2]
+            path = chomp(split(line, "=")[2])
+            write(cenv, "ENV[\"$var\"] = \"$path\"\n")
           end
         end
       end
+      cd(here) do
+        isdir("files") && rm("files", recursive=true)
+        mkdir("files")
+      end
+      path = joinpath(here, "files", "problems")
+      println(cenv, "ENV[\"cutest-problems\"] = \"$path\"")
     end
   end
 end
